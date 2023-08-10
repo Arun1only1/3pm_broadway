7c00edd37b2d7fdca55d1cfb845f06aa7980d350
Arun1only1 <arun1nly1@gmail.com>
2023-08-10 16:19:09 +0545

    feat:student edit and get all students

--- student/student.route.js
+++ student/student.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { checkMongoIdValidity } from "../utils/utils.js";
 import { Student } from "./student.model.js";
 import {
   checkIfStudentExists,
   deleteStudent,
   getStudentDetails,
 } from "./student.service.js";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
@@ -27,5 +27,49 @@ router.get("/student/details/:id", checkIfStudentExists, getStudentDetails);
 
 // delete a student
 router.delete("/student/delete/:id", checkIfStudentExists, deleteStudent);
+
+// edit a student
+router.put("/student/edit/:id", async (req, res) => {
+  // extract id from params
+  const studentId = req.params.id;
+
+  // validate id so that it is valid mongId
+  const isValidMongoId = mongoose.Types.ObjectId.isValid(studentId);
+
+  // if not valid,throw error
+
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Invalid mongo id." });
+  }
+
+  // check if user exists of that id
+  const student = await Student.findOne({ _id: studentId });
+
+  // if not user, throw error
+  if (!student) {
+    return res.status(404).send({ message: "Student does not exist." });
+  }
+
+  // edit user with data from req.body
+  const newData = req.body;
+  await Student.updateOne(
+    { _id: studentId },
+    {
+      name: newData.name,
+      age: newData.age,
+      gender: newData.gender,
+      location: newData.location,
+    }
+  );
+  // send appropriate response
+  return res.status(200).send({ message: "Student is updated successfully." });
+});
+
+// get all students
+router.get("/students", async (req, res) => {
+  const allStudents = await Student.find();
+
+  return res.status(200).send(allStudents);
+});
 
 export { router };
